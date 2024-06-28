3775b15a8bd4c6893481831072500ce6a9ba99c1
Pratik5295 <36683380+Pratik5295@users.noreply.github.com>
2024-06-28 23:40:45 +0530

    Grid trial run

---
+++ DevProject.js
@@ -0,0 +1,58 @@
+
+async function fetchTemplate() 
+{
+const response = await fetch('ProjectTemplate.html');
+return response.text();
+}
+
+async function loadContent(file) 
+{
+try {
+    const response = await fetch(file);
+    const data = await response.json();
+
+    const template = await fetchTemplate();
+   
+    return template;
+
+} catch (error) {
+    console.error('Error loading content:', error);
+}
+}
+
+async function LoadMultipleFiles(filepaths)
+{
+    const allData = [];
+
+    for(const filepath in filepaths)
+        {
+            const data = await loadContent(filepath);
+            allData.push(...data);
+        }
+
+        return allData;
+}
+
+async function fetchDataAndFillGrid() 
+{
+    console.log("Fetching data and filling grid...");
+    const gridContainer = document.getElementById('grid-container');
+    const template = document.getElementById('grid-item-template').content;
+
+    const filePaths = ['tower-defense.txt','demon-spawn.txt'];
+    const data = await LoadMultipleFiles(filePaths);
+
+    data.forEach(item => {
+        const constructedPage = template
+        .replace('{{title}}', item.title)
+        .replace('{{content}}', item.content)
+        .replace('{{videoTitle}}',item.videoTitle)
+        .replace('{{videoUrl}}',item.videoUrl);
+        gridContainer.appendChild(constructedPages);
+    });
+}
+
+window.onload = () =>
+{
+    fetchDataAndFillGrid();
+};
