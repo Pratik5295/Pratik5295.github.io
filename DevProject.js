f4e2e5f16cb0b45535ab46844f35b6e8080e8420
Pratik5295 <36683380+Pratik5295@users.noreply.github.com>
2024-07-03 01:22:06 +0530

    Updating some grid cells on index page. Has duplicates for testing

--- DevProject.js
+++ DevProject.js
@@ -43,7 +43,7 @@ async function fetchDataAndFillGrid() {
     const gridContainer = document.getElementById('grid-container');
     const template = await fetchTemplate('Homepage-Project-Template.html');
 
-    const filePaths = ['tower-defense.txt','demon-spawn.txt']; // List of text file paths containing JSON
+    const filePaths = ['tower-defense.txt','demon-spawn.txt','demon-spawn.txt','tower-defense.txt','demon-spawn.txt','demon-spawn.txt']; // List of text file paths containing JSON
     const data = await fetchDataFromMultipleFiles(filePaths);
 
     data.forEach(item => {
