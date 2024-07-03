34053292edd093d8a6ca3d04a1be5a25082b3645
Pratik5295 <36683380+Pratik5295@users.noreply.github.com>
2024-07-03 16:33:03 +0530

    Art section pass 1: Grid based 4 items and art station link with paragraph

--- Scripts/HtmlLoader.js
+++ Scripts/HtmlLoader.js
@@ -3,5 +3,6 @@ async function LoadPage(path,id)
     const response = await fetch(path);
     const text = await response.text();
     document.getElementById(id).innerHTML = text;
+    console.log("Loaded page: " + path);
 }
 
