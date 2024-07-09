4cab17e703a33d1c141004ea8ad76dc2bf2544c6
Pratik5295 <162622868+Pratik-Guppy@users.noreply.github.com>
2024-07-09 18:12:38 +0530

    Cube craft and Tile Editor tool

--- DevProject.js
+++ DevProject.js
@@ -41,7 +41,7 @@ async function fetchDataAndFillGrid() {
     const gridContainer = document.getElementById('grid-container');
     const template = await fetchTemplate('Homepage-Project-Template.html');
 
-    const filePaths = ['tower-defense.txt','demon-spawn.txt','demon-spawn.txt','tower-defense.txt','demon-spawn.txt','demon-spawn.txt']; // List of text file paths containing JSON
+    const filePaths = ['tower-defense.txt','demon-spawn.txt','tiletool.txt','cube-craft.txt']; // List of text file paths containing JSON
     const data = await fetchDataFromMultipleFiles(filePaths);
 
     data.forEach(item => {
